ee628c7762ca919ec57d98b372f68dfd272f3e95
Dennis Seidel <den.seidel@gmail.com>
2020-05-12 18:11:40 +0200

    feat: store input from input elements on class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'healthinsurance-price-calculator';
+  coverageStart = '';
+  coverageScope = '';
+  zip = '';
+  birthdate = '';
   price = '';
+
+  onChangeCoverageStart(value: string) {
+    this.coverageStart = value;
+  }
+
+  onChangeCoverageScope(value: string) {
+    this.coverageScope = value;
+  }
+
+  onChangeZip(value: string) {
+    this.zip = value;
+  }
+
+  onChangeBirthdate(value: string ) {
+    this.birthdate = value;
+  }
 
   onButtonClick() {
     this.price = '40';
