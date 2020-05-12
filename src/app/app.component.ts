c43aeb9fb2d93f4a681fb60c1658248021077a6b
Dennis Seidel <den.seidel@gmail.com>
2020-05-12 19:26:33 +0200

    feat: display product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   zip = '';
   birthdate = '';
   governmentSubside = false;
-  price = '';
+  products = [];
 
   onChangeCoverageStart(value: string) {
     this.coverageStart = value;
@@ -33,7 +33,47 @@ export class AppComponent {
     this.governmentSubside = !this.governmentSubside;
   }
 
+  getProducts(coverageStart: string, coverageScope: string, zip: string, birthdate: string, governmentSubside?: boolean) {
+    return {
+      products: [
+        {
+          name: 'Simple Bronze LOWEST PREMIUM',
+          desciption: 'If you rarely see a doctor, this low-cost plan could work for you.',
+          use: 'Basic Use: Good for infrequent medical needs',
+          outOfPocketMaximum: '$8,150',
+          deductable: '$8,150',
+          monthlyPremium: '$533.88'
+        },
+        {
+          name: 'Saver Bronze',
+          desciption: 'If you have minimal health care needs, this plan could work for you.',
+          use: 'Basic Use: Good for infrequent medical needs',
+          outOfPocketMaximum: '$7,200',
+          deductable: '$7,200',
+          monthlyPremium: '$536.74'
+        },
+        {
+          name: 'Classic Silver BEST SELLING',
+          desciption: 'Great plan if you want affordable prescription drugs starting day 1.',
+          use: 'Regular Use: Good for occasional medical needs',
+          outOfPocketMaximum: '$7,900',
+          deductable: '$1,300',
+          monthlyPremium: '$728.12'
+        },
+        {
+          name: 'Classic Gold',
+          desciption: 'This plan has a reasonable deductible and great prescription drugs benefits.',
+          use: 'Frequent Use: Good for high medical needs',
+          outOfPocketMaximum: '$4,000',
+          deductable: '$600',
+          monthlyPremium: '$919.47'
+        },
+      ]
+    }
+  }
+
   onButtonClick() {
-    this.price = '40';
+    this.products = this.getProducts(this.coverageStart, this.coverageScope, this.zip, this.birthdate, this.governmentSubside).products;
+    console.log(this.products)
   }
 }
