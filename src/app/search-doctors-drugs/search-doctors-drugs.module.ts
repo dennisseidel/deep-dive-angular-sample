399baa8557468085e754535fde5a4fd72399f27a
Dennis Seidel <den.seidel@gmail.com>
2020-07-05 14:53:51 +0200

    feat: add routing&routed modules

---
+++ src/app/search-doctors-drugs/search-doctors-drugs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchDoctorsDrugsRoutingModule } from './search-doctors-drugs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SearchDoctorsDrugsRoutingModule
+  ]
+})
+export class SearchDoctorsDrugsModule { }
---
+++ src/app/shop-plans/shop-plans.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShopPlansRoutingModule } from './shop-plans-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ShopPlansRoutingModule
+  ]
+})
+export class ShopPlansModule { }
