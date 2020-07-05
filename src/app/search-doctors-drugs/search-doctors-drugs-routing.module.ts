7531c20ec304cf3c7abd0bd1d967ed6a952b144e
Dennis Seidel <den.seidel@gmail.com>
2020-07-05 15:17:27 +0200

    feat: define the route

--- src/app/search-doctors-drugs/search-doctors-drugs-routing.module.ts
+++ src/app/search-doctors-drugs/search-doctors-drugs-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SearchDoctorsDrugsHomeComponent } from './search-doctors-drugs-home/search-doctors-drugs-home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: 'search',
+  component: SearchDoctorsDrugsHomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
