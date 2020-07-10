ef904c1dda7b5697fb30a806db22534c2c222301
Dennis Seidel <den.seidel@gmail.com>
2020-07-10 15:00:23 +0200

    feat: generate SearchBar & ResultList components

--- src/app/search-doctors-drugs/search-doctors-drugs.module.ts
+++ src/app/search-doctors-drugs/search-doctors-drugs.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { SearchDoctorsDrugsRoutingModule } from './search-doctors-drugs-routing.module';
 import { SearchDoctorsDrugsHomeComponent } from './search-doctors-drugs-home/search-doctors-drugs-home.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ResultListComponent } from './result-list/result-list.component';
 
 
 @NgModule({
-  declarations: [SearchDoctorsDrugsHomeComponent],
+  declarations: [SearchDoctorsDrugsHomeComponent, SearchBarComponent, ResultListComponent],
   imports: [
     CommonModule,
     SearchDoctorsDrugsRoutingModule
